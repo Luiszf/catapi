c5d01f8f18009d88817e67f5582c23d9c4ff2249
luis <lfernando.s.nascimento@gmail.com>
2023-11-13 21:13:45 -0300

    upload and download of images in bytes with compression and decompression implemented, but with no integration with the current post entity.

--- src/main/java/com/luis/simplerestapi/CatApiApplication.java
+++ src/main/java/com/luis/simplerestapi/CatApiApplication.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.awt.*;
+
 @SpringBootApplication
 public class CatApiApplication {
 
---
+++ src/main/java/com/luis/simplerestapi/service/ImageDataService.java
@@ -0,0 +1,13 @@
+package com.luis.simplerestapi.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface ImageDataService {
+
+    public String uploadImage(MultipartFile file) throws IOException;
+
+
+    public byte[] dowloadImage(Long id);
+}
